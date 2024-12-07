d9399cb1c07943f2d97e1ae76114fc9be2b5308b
JeldrikH <jeldrik.hartmann@web.de>
2024-12-08 00:34:00 +0100

    test commit

--- Scenes/Main/Buildings/cauldron.gd
+++ Scenes/Main/Buildings/cauldron.gd
@@ -5,6 +5,7 @@ extends Building
 func _ready() -> void:
 	super._ready()
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	super._process(delta)
